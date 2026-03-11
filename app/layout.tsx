3197d504185a5f2c291c29c54e87ceaea285c711
jumboslice09 <johngriffinbell2@gmail.com>
2026-03-11 12:34:05 -0500

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <body style={{fontFamily:"Arial"}}>
+        {children}
+      </body>
+    </html>
+  )
+}
